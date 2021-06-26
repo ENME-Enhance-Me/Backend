9ae3fda7bcbf2f3ab69ab31157fb2398dc62d5f4
jotalima72 <48960188+jotalima72@users.noreply.github.com>
2021-06-26 03:24:29 -0300

    Migrations for Brand
    adicionando marcas ao banco de dados com foreign key para usuarios

---
+++ src/shared/migration/1624686351188-CreateBrandTable.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateBrandTable1624686351188 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'brand',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'company_name',
+            type: 'varchar',
+          },
+          {
+            name: 'CNPJ_CPF',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('brand');
+  }
+}
