e4e6e0fd965a2204c08e6b1e640cbb1d12dd64ca
jotalima72 <48960188+jotalima72@users.noreply.github.com>
2021-07-01 16:31:07 -0300

    Create ClientsTable and alter UserTable in database

---
+++ src/shared/migration/1625167430520-AddUserIDFKtoClients.ts
@@ -0,0 +1,23 @@
+import {MigrationInterface, QueryRunner, TableForeignKey} from "typeorm";
+
+export class AddUserIDFKtoClients1625167430520 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createForeignKey(
+            'clients',
+            new TableForeignKey({
+              name: 'userFKClients',
+              columnNames: ['userID'],
+              referencedTableName: 'user',
+              referencedColumnNames: ['id'],
+              onDelete: 'CASCADE',
+              onUpdate: 'CASCADE',
+            }),
+          );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('clients', 'userFK');
+    }
+
+}
