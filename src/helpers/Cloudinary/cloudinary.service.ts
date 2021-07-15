5b8d1579800bcce6996b1827dc05fbdd39d44b51
jotalima72 <48960188+jotalima72@users.noreply.github.com>
2021-07-15 17:55:03 -0300

    tratamento de erro na inserção e função de deleção de imagem

--- src/helpers/Cloudinary/cloudinary.service.ts
+++ src/helpers/Cloudinary/cloudinary.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
+import { UploadApiResponse, v2 } from 'cloudinary';
 import { FileUpload } from 'graphql-upload';
-import { Stream } from 'stream';
+import { Cloudinary } from './cloudinary';
 
 
 @Injectable()
@@ -13,18 +13,30 @@ export class CloudinaryService {
     folderName: string
   ): Promise<UploadApiResponse> {
     return new Promise((resolve, reject) => {
-        const upload = v2.uploader.upload_stream({
-            allowed_formats: ["jpg", "png"],
-            folder: folderName
-        },(error, result) => {
-            if (error) return reject(error);
-            resolve(result);
-            
-        }); 
+      const upload = v2.uploader.upload_stream({
+        allowed_formats: ["jpg", "png"],
+        folder: folderName
+      }, (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+
+      });
+      try {
         file.createReadStream().pipe(upload)
-        .on('finish', () => "true")
-        .on('error', () => {throw new BadRequestException('Erro no pipe.');})
+          .on('finish', () => "true")
+          .on('error', () => { throw new BadRequestException('Erro no pipe.'); });
+      }
+      catch (err) {
+        throw new BadRequestException('imagem inexistente ou formato errado.');
+      }
     });
 
+  }
+  async deleteImage(link: string) {
+    const cloud = require('cloudinary').v2;
+    cloud.config(Cloudinary.useFactory());
+    const a = cloud.uploader.destroy(link, function(error,result) {
+      console.log(result, error) });
+    return a;
   }
 }
